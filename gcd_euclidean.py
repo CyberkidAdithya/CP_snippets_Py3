5af27460b824e846ea1a143290386ce04d98cc56
cyberkid05 <cyberadithya05@gmail.com>
2021-12-31 15:54:53 +0530

    Update gcd_euclidean.py

--- gcd_euclidean.py
+++ gcd_euclidean.py
@@ -2,10 +2,10 @@ def gcd_iter(a, b):
     while(b):
         a, b = b, a % b
     return a
-# gcd_iter(13, 19)
+# print(gcd_iter(13, 19))
 
 def gcd_recur(a, b):
     if b == 0:
         return a
     return gcd(b, a % b)
-# gcd_recur(13, 19)
+# print(gcd_recur(13, 19))
