f97b1811a5915eb5f6d6e98bc5c457a44e2919f5
cyberkid05 <cyberadithya05@gmail.com>
2021-12-31 15:56:38 +0530

    Update binary_exponentiation.py

--- binary_exponentiation.py
+++ binary_exponentiation.py
@@ -8,7 +8,7 @@ def findpow_iter(a, b, m = int(1e9 + 7)):    #iterative
         a = a * a % m
         b >>= 1
     return res % m
-# findpow_iter(x, y, modd)
+# print(findpow_iter(243, 4757))
 
 def findpow_recur(a, b, m = int(1e9 + 7)):    #recursive
     if a == 0:
@@ -20,4 +20,4 @@ def findpow_recur(a, b, m = int(1e9 + 7)):    #recursive
         return tmp * tmp % m
     else:
         return ((tmp * tmp % m) * a) % m
-# findpow_recur(x, y, modd)
+# print(findpow_iter(243, 4757, 100007))
