e16dc133e5892286a5edaaae14df1ff2bf55cfe3
cyberkid05 <cyberadithya05@gmail.com>
2022-01-01 09:59:12 +0530

    Create binary_tree.py

---
+++ binary_tree.py
@@ -0,0 +1,48 @@
+class Node:
+   def __init__(self, data):
+      self.left = None
+      self.right = None
+      self.val = data
+
+class Solution:
+    def inorderTraversal(self, root):
+        nodes = []
+        def TraversalUtil(root):
+            if not root:
+                return
+            
+            # for child in root.children:
+            #     TraversalUtil(child)
+            
+            if root.left:
+                TraversalUtil(root.left)
+                
+            nodes.append(root.val)
+                
+            if root.right:
+                TraversalUtil(root.right)
+                
+        TraversalUtil(root)
+        return nodes
+
+'''   Let us create below tree
+*              5
+*           //    \\
+*          3        7
+*        // \\    // \\
+*        1   4    6   8
+*           //
+*          2
+'''
+arr = Node(5)
+arr.left = Node(3)
+arr.left.right = Node(4)
+arr.left.left = Node(1)
+arr.right = Node(7)
+arr.right.right = Node(8)
+arr.right.left = Node(6)
+arr.left.right.left = Node(2)
+obj = Solution()
+ans = obj.inorderTraversal(arr)
+print(ans)
+# [1, 3, 2, 4, 5, 6, 7, 8]
